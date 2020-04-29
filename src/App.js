ddf87d0cf56d4d5d1b867a85123d4225873d90b4
farzad2004 <farzad.alamdar2008@gmail.com>
2020-04-30 01:10:08 +1000

    lecture 32

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ class App extends Component {
     super();
     this.state = {
       monsters: [],
+      searchField: "",
     };
   }
   componentDidMount() {
@@ -19,6 +20,13 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <input
+          type="search"
+          placeholder="search monsters"
+          onChange={(e) => {
+            this.setState({ searchField: e.target.value });
+          }}
+        />
         <CardList monsters={this.state.monsters} />
       </div>
     );
